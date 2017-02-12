8bce3576978db8c7453dbf2eb188327b335891b7
Marin Rados <marinrados@Marins-MacBook-Pro.local>
2017-02-12 20:20:39 +0100

    character, walls, enemy movement

---
+++ Assets/Weapon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+	public float fireRate = 0;
+	public float Damage = 10;
+	public LayerMask whatToHit;
+
+	public Transform bulletTrailPrefab;
+
+	public float effectSpawnRate = 10;
+
+	float timeToSpawnEffect = 0;
+	Transform firePoint;
+
+
+	// Use this for initialization
+	void Awake () {
+		firePoint = transform.FindChild ("FirePoint");
+		if (firePoint == null) {
+			Debug.LogError ("No fire point");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fireRate == 0) {
+			if (Input.GetButtonDown ("Fire1")) {
+				Shoot ();
+			}
+		}
+	}
+
+	void Shoot () {
+		Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
+			Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
+		RaycastHit2D hit = Physics2D.Raycast (firePointPosition, mousePosition-firePointPosition, 100, whatToHit);
+		if (Time.time >= timeToSpawnEffect) {
+			Effect ();
+			timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+		}
+		if (hit.collider != null) {
+			Debug.Log ("We hit something");
+		}
+	}
+
+	void Effect() {
+		Instantiate (bulletTrailPrefab, firePoint.position, firePoint.rotation);
+	}
+}
